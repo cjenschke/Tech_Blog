769408a8c0c6c5c71e22d48a6ba3c26319ed371e
cjenschke <cimarron13@mac.com>
2023-11-08 17:03:07 -0600

    User.js initial code

---
+++ db/models/User.js
@@ -0,0 +1,56 @@
+// Import the necessary dependencies
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Define the User model
+class User extends Model {}
+
+// Set up the fields and rules for the User model
+User.init(
+    {
+        // Define the user model attributes
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate:{
+                isEmail: true,
+            },
+        },
+        password:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize, // Pass in the sequelize connection
+        timestamps: false, // Disable timestamps for this model
+        freezeTableName: true, // Prevent Sequelize from pluralizing the table name
+        underscored: true, // Use underscores for field names
+        modelName: 'user', // Set the model name to 'user'
+    }
+);
+
+// Create associations with other models, if necessary
+User.hasMany(Post,{
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE', // Define the behavior on user deletion
+});
+
+User.hasMany(Comment,{
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+// Export the User model
+module.exports = User,
