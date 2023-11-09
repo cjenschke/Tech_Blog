0967fd769d0c28b61ad405ea554d631b3559c742
cjenschke <cimarron13@mac.com>
2023-11-08 18:15:23 -0600

    models/index.js code added

---
+++ db/models/index.js
@@ -0,0 +1,32 @@
+// Import your model files
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// Define associations between models
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
+
+// Export object that contains all models
+module.exports = { User, Post, Comment };
